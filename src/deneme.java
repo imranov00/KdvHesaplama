4713a830a1e919444f25348bed3210db2030275a
İmranov <semtexpert@gmail.com>
2023-12-19 18:41:17 +0300

    versiyon 12

---
+++ src/deneme.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class deneme {
+    public static void main(String[] args) {
+        double kdvorani, tutar,kdvlifiyat, kdvtutarı;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Tutar Giriniz:");
+        tutar = input.nextDouble();
+
+
+
+
+
+        kdvorani = (tutar >= 0 && tutar <= 1000) ? 0.18 : 0.8;
+        kdvtutarı= tutar*kdvorani;
+        kdvlifiyat= tutar + kdvtutarı;
+        System.out.println("Kdv Oranı:"+kdvorani);
+
+        System.out.println("KDV`li Fiyat:"+ kdvlifiyat);
+
+        System.out.println("KDV Tutarı:" + kdvtutarı);
+
+
+
+    }
+}
+
+
